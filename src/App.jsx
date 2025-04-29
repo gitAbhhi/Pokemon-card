3e45ebdadde39f05166b25e81cc60c42ad3ecd60
gitAbhhi <140035168+gitAbhhi@users.noreply.github.com>
2025-04-29 18:39:37 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,14 @@ import Footerpage from './component/Footer'
 import Productdetail from './pages/Productdetail'
 import Login from './pages/Login'
 import Home from './pages/Home'
-import { StoreContext } from './component/context/StoreContext'
-import Cart from './pages/Cart'
-import { ToastContainer } from 'react-toastify'
-import "react-toastify/dist/ReactToastify.css"
-import ProtectedRoute from './component/ProtectedRoute'
 
 
 
 function App() {
-  const { token } = useContext(StoreContext);
-  const navigate = useNavigate();
   return (
 <>
-      <ToastContainer />
       <Routes>
-        {/* Public Route */}
-        <Route path="/login" element={<Login />} />
-
-        {/* Protected Routes */}
-        <Route element={<ProtectedRoute />}>
           <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/product/:id" element={<Productdetail />} />
-        </Route>
       </Routes>
     </>
 
